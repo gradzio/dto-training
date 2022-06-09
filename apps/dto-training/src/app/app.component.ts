2e6d0916f30a3c2ab8650912d93957eeef331f9e
gradzio <gregradzio@gmail.com>
2022-06-09 07:35:17 +0200

    initial commit

---
+++ apps/dto-training/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dto-training-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dto-training';
+}
